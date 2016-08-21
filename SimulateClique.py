82943a36b7126ee3553adf0fdd5acce951c97e93
Przemyslaw Kowalczyk <pirtim.pk@gmail.com>
2016-08-21 09:19:54 +0200

    Added lazy simulation.

--- SimulateClique.py
+++ SimulateClique.py
@@ -1,4 +1,5 @@
 # -*- coding: utf-8 -*-
+# Keep in mind that this code is based on old software from summer 2015 when i didn't know python well.
 from __future__ import division #~ Domysle dzielenie int jako liczb float
 from igraph import *     #~ Do symulacji grafu
 import random            #~ Do wybierania losowych elementow z listy
@@ -24,19 +25,19 @@ def wypiszDane(g, KlikList, stg):
     print "ilosci wierzcholkow:", g.vcount()
     print "ilosci polaczen:", g.ecount()
     print "sredniego stopnia:", round(mean(g.degree()), 2)
-    print "ilosci klik:", len(KlikList)
-    print "wielkosc klik:", stg['CONST_CLIQUE']
+    if stg['CONST_MODEL'] == 'clique':
+        print "ilosci klik:", len(KlikList)
+        print "wielkosc klik:", stg['CONST_CLIQUE']
     if 'CONST_START_MAGNETIZATION' in stg:
         print "start_mag:", stg['CONST_START_MAGNETIZATION']
 
-
 def zapisywanie_danych(j, g, M, Mlist, KlikList, stg):
-
     #~ Dane do dodatkowego pliku opisujacego zapisane dane
     daneWrite = stg.copy()
-    daneWrite['WYN_meanG'] = round(mean(g.degree()), 2)
+    daneWrite['WYN_MEAN_k'] = round(mean(g.degree()), 2)
     daneWrite['WYN_j'] = j
-    daneWrite['WYN_len(KlikList)'] = len(KlikList)
+    if stg['CONST_MODEL'] == 'clique':
+        daneWrite['WYN_len(KlikList)'] = len(KlikList)
     daneWrite['WYN_M'] = M
 
     # daneWrite = '''CONST_VERTICES\n{}\nCONST_EDGES\n{}\nCONST_SIM_COUNT\n{}\nCONST_PRINT\n{}\nCONST_TIME\n{}\nmeanG\n{}\nj\n{}\nlen(KlikList)\n{}\nM\n{}\nwielkosc klik\n{}'''.format(stg['CONST_VERTICES'], stg['CONST_EDGES'], stg['CONST_SIM_COUNT'], stg['CONST_PRINT'], stg['CONST_TIME'], round(mean(g.degree()), 2),j, len(KlikList), M, stg['CONST_CLIQUE'])
@@ -44,25 +45,34 @@ def zapisywanie_danych(j, g, M, Mlist, KlikList, stg):
     #~ Stale folderowo-plikowe
     CONST_STANDARD_PATH = stg['CONST_PATH_BASIC_FOLDER']
 
-    if 'CONST_START_MAGNETIZATION' in stg:
-        CONST_STANDARD_RAW_PATH = os.path.join(CONST_STANDARD_PATH, 'RawDataMag', 'klik' + str(stg['CONST_CLIQUE']), 'mag_start', '{:0<5}'.format(stg['CONST_START_MAGNETIZATION']))
-        # CONST_STANDARD_WYK_PATH = os.path.join(CONST_STANDARD_PATH, 'Wykresy', 'klik' + str(stg['CONST_CLIQUE']), 'mag_start', '{:0<5}'.format(stg['CONST_START_MAGNETIZATION']))
-        CONST_STANDARD_WYK_PATH = os.path.join(CONST_STANDARD_PATH, 'Wykresy', 'klik' + str(stg['CONST_CLIQUE']))#, 'mag_start', '{:0<5}'.format(stg['CONST_START_MAGNETIZATION']))
+    if stg['CONST_MODEL'] == 'clique': 
+        if 'CONST_START_MAGNETIZATION' in stg:
+            CONST_STANDARD_RAW_PATH = os.path.join(CONST_STANDARD_PATH, 'RawDataMag', 'klik' + str(stg['CONST_CLIQUE']), 'mag_start', '{:0<5}'.format(stg['CONST_START_MAGNETIZATION']))
+            CONST_STANDARD_WYK_PATH = os.path.join(CONST_STANDARD_PATH, 'Wykresy', 'klik' + str(stg['CONST_CLIQUE']))#, 'mag_start', '{:0<5}'.format(stg['CONST_START_MAGNETIZATION']))
+        else:
+            CONST_STANDARD_RAW_PATH = os.path.join(CONST_STANDARD_PATH, 'RawDataMag', 'klik' + str(stg['CONST_CLIQUE']), 'stopnie', str(stg['CONST_MEAN_k']))
+            CONST_STANDARD_WYK_PATH = os.path.join(CONST_STANDARD_PATH, 'Wykresy', 'klik' + str(stg['CONST_CLIQUE']), 'stopnie',  str(stg['CONST_MEAN_k']))
+    elif stg['CONST_MODEL'] == 'lazy':
+        CONST_STANDARD_RAW_PATH = os.path.join(CONST_STANDARD_PATH, 'RawDataMag', 'val_start', '{:0<5}'.format(get_model_val(stg)))
+        CONST_STANDARD_WYK_PATH = os.path.join(CONST_STANDARD_PATH, 'Wykresy')
     else:
-        CONST_STANDARD_RAW_PATH = os.path.join(CONST_STANDARD_PATH, 'RawDataMag', 'klik' + str(stg['CONST_CLIQUE']), 'stopnie', str(stg['CONST_MEAN_k']))
-        CONST_STANDARD_WYK_PATH = os.path.join(CONST_STANDARD_PATH, 'Wykresy', 'klik' + str(stg['CONST_CLIQUE']), 'stopnie',  str(stg['CONST_MEAN_k']))
-
+        ValuerError('Model not found.')
+            
     CONST_PATH_FILES_NUMBER = os.path.join(CONST_STANDARD_RAW_PATH, 'filesCount.txt')
     CONST_GENERATED_FILES    = 2
     
-    SPECIALdirectoryRAW = os.path.join(CONST_STANDARD_RAW_PATH, 'k' + str(int(round(Mlist[-1],0))))
+    if stg['CONST_MODEL'] == 'lazy':
+        SPECIALdirectoryRAW = CONST_STANDARD_RAW_PATH
+    else:
+        SPECIALdirectoryRAW = os.path.join(CONST_STANDARD_RAW_PATH, 'k' + str(int(round(Mlist[-1],0))))
     CheckFolder(SPECIALdirectoryRAW)
     
     if stg['CONST_OVERRIDEN']:
         fileNumber = i
     elif os.path.isfile(CONST_PATH_FILES_NUMBER):
         with open(CONST_PATH_FILES_NUMBER, 'r+') as f_n:
-            fileNumber = eval(f_n.read())
+            # fileNumber = eval(f_n.read())
+            fileNumber = int(f_n.read())
             f_n.seek(0) #~ Bufor na poczatek pliku
             f_n.write(str(fileNumber + 1))
     else:
@@ -77,9 +87,12 @@ def zapisywanie_danych(j, g, M, Mlist, KlikList, stg):
     
     CheckFolder(CONST_STANDARD_WYK_PATH)
     if 'CONST_START_MAGNETIZATION' in stg:
-        filepathWYK = os.path.join(CONST_STANDARD_WYK_PATH, 'mag_start{:0<5} file_nb{}.png'.format(stg['CONST_START_MAGNETIZATION'], fileNumber))
+        filepathWYK = os.path.join(CONST_STANDARD_WYK_PATH, 'model_val{:0<5} file_nb{}.png'.format(get_model_val(stg), fileNumber))
     else:
         filepathWYK = os.path.join(CONST_STANDARD_WYK_PATH, 'Magnetyzacja'+ str(fileNumber) +'.png')
+
+
+    filepathWYK = os.path.join(CONST_STANDARD_WYK_PATH, 'model_val{:0<5} file_nb{}.png'.format(get_model_val(stg), fileNumber))
 
     #~ Plotowanie
     plt.plot(Mlist) #~ Tworzenie wykresow magnetyzacji
@@ -90,7 +103,7 @@ def zapisywanie_danych(j, g, M, Mlist, KlikList, stg):
     plt.savefig(filepathWYK, bbox_inches='tight', dpi = 300)
     plt.clf()
 
-def zapisz_magnetyzacje_i_end_check(j, Mlist, g, stg):
+def zapisz_magnetyzacje_i_end_check_clique(j, Mlist, g, stg):
     if j % (stg['CONST_VERTICES']//100) == 0:#~ 1000) == 0: 
         #~ Zapisywanie magnetyzacji raz na jakis czas
         M = Magnetyzacja(g, stg)    #~ "Ciezkie" obliczenie czasowo - tyle ile reszta MCS
@@ -101,7 +114,7 @@ def zapisz_magnetyzacje_i_end_check(j, Mlist, g, stg):
 
 def Magnetyzacja(g, stg):
     'Oblicza magnetyzacje grafu g.'
-    return sum(g.vs["stan"])/stg['CONST_VERTICES']
+    return sum(g.vs["stan"])/stg['WYN_REAL_VERTICES']
 
 def czySaKliki(KlikList):
     return len(KlikList) > 0
@@ -110,7 +123,8 @@ def inicjalizacja(stg):
     #~ Tworzenie grafu losowego Erdos_Renyi
     g = Graph.Erdos_Renyi(n=stg['CONST_VERTICES'], m=stg['CONST_EDGES']) 
     #~ Usuniecie izolowanych wierzcholkow      
-    g.delete_vertices(g.vs.select(_degree=0))    
+    g.delete_vertices(g.vs.select(_degree=0))
+    stg['WYN_REAL_VERTICES'] = len(g.vs)
     #~ Obsadzanie kazdego wezla losowym spinem
     if 'CONST_START_MAGNETIZATION' in stg:
         g.vs['stan'] = np.array(np.random.random_sample(stg['CONST_VERTICES']) > (1-stg['CONST_START_MAGNETIZATION']), np.int)
@@ -124,11 +138,15 @@ def inicjalizacja(stg):
     #~ licznik po ilosci MCS 
     j = 0
     #~ Lista z klikami   
-    KlikList = g.cliques(min = stg['CONST_CLIQUE'], max = stg['CONST_CLIQUE']) 
+    if stg['CONST_MODEL'] == 'clique':
+        KlikList = g.cliques(min = stg['CONST_CLIQUE'], max = stg['CONST_CLIQUE'])
+    elif stg['CONST_MODEL'] == 'lazy':
+        KlikList = None
+    
     return j, g, M, Mlist, KlikList
 
 
-def MCS_steps(j, g, Mlist,  KlikList, stg):    
+def MCS_steps_clique(j, g, Mlist,  KlikList, stg):    
     'Glowna petla jednego kroku (MCS)'
     while True: 
         ranTroj = random.choice(KlikList) #~ Wybieramy losowa trojke
@@ -153,8 +171,45 @@ def MCS_steps(j, g, Mlist,  KlikList, stg):
 			# TODO: zaznaczanie ze dana czesc grafu nie musi byc sprawdzana i zmieniana w jakiejsc zmienne edga, jak to potem odwrocic?
 
         j += 1 #~ Licznik MCS
-        if zapisz_magnetyzacje_i_end_check(j, Mlist, g, stg): break #~ Wyjscie z MCS
+        if zapisz_magnetyzacje_i_end_check_clique(j, Mlist, g, stg): break #~ Wyjscie z MCS
     return j
+
+def zapisz_magnetyzacje_i_end_check_lazy(j, Mlist, g, stg):
+    if j % stg['CONST_VERTICES']//100 == 0: #~ Zapisywanie magnetyzacji raz na jakis czas
+        M = Magnetyzacja(g, stg)     #~ "Ciezkie" obliczenie czasowo - tyle ile reszta MCS       
+        Mlist.append(M)
+        if stg['CONST_PRINT']: print 'Magnetyzacja',j,' : ', M
+        var1 = j > stg['CONST_VERTICES']*stg['CONST_SIM_LONG']
+        var2 = False
+        # var2 = not ((M > stg['CONST_LAZY_CUT']) and (M < (1-stg['CONST_LAZY_CUT'])))
+        return var1 or var2
+
+def MCS_steps_lazy(j, g, Mlist,  KlikList, stg):    
+    'Glowna petla jednego kroku (MCS)'    
+    while True: 
+        ranEdge = random.choice(g.es) #~ Wybieramy losowa krawedz        
+        A = ranEdge.source #~ Pierwszy wierzcholek krawedzi
+        B = ranEdge.target #~ Drugi wierzcholek krawedzi
+        
+        if (g.degree(g.vs[A]) > 1) and (g.degree(g.vs[B]) > 1):            
+            Ap = B    #~ <- po liczbach a nie obiektach
+            Bp = A                
+            while Ap == B: #~ Losowanie sasiada A'
+                Ap = random.choice(g.neighbors(g.vs[A]))
+            while Bp == A: #~ Losowanie sasiada B'
+                Bp = random.choice(g.neighbors(g.vs[B]))
+            if Ap == Bp:
+                continue            
+            #~ print A, B, Ap, Bp
+            #~ print g.vs[Ap]["stan"], g.vs[A]["stan"], g.vs[B]["stan"], g.vs[Bp]["stan"]            
+            g.vs[Ap]["stan"] = g.vs[B]["stan"] #~ przypisanie nowych wartosci
+            g.vs[Bp]["stan"] = g.vs[A]["stan"] #~ zgodnie z modelem sznajdow    
+            j = j + 1
+			# TODO: zaznaczanie ze dana czesc grafu nie musi byc sprawdzana i zmieniana w jakiejsc zmienne edga, jak to potem odwrocic?
+            if zapisz_magnetyzacje_i_end_check_lazy(j, Mlist, g, stg): break #~ Wyjscie z MCS            
+
+def get_model_val(stg):
+    return stg[stg['CONST_MODEL_BASIC_VAL']]
 
 def jedna_symulacja(stg):
     'Petla po powtorzeniach symulacji dla roznych grafow'
@@ -162,10 +217,15 @@ def jedna_symulacja(stg):
         print '\nRozpoczynam symulacje nr:', i, 'dla: '
         j, g, M, Mlist, KlikList = inicjalizacja(stg)     
         wypiszDane(g, KlikList, stg)
-        if not czySaKliki(KlikList):
-            print 'Brak klik - Koniec'
-            break
-        j = MCS_steps(j, g, Mlist,  KlikList, stg)
+        if stg['CONST_MODEL'] == 'clique':
+            if not czySaKliki(KlikList):
+                print 'Brak klik - Koniec'
+                break
+            j = MCS_steps_clique(j, g, Mlist,  KlikList, stg)
+        elif stg['CONST_MODEL'] == 'lazy':
+            j = MCS_steps_lazy(j, g, Mlist,  KlikList, stg)
+        else:
+            raise ValueError('Wrong model type.')
         print 'Magnetyzacja koncowa', j, ' : ', Mlist[-1]    
         zapisywanie_danych(j, g, M, Mlist, KlikList, stg)
         del g
@@ -178,19 +238,22 @@ if __name__ == '__main__':
     rc('font', family='Arial') #Plotowanie polskich liter
     #~ Definicje stalych symulacji
     stg = {
-        'CONST_CLIQUE'    : 3,      #~ Wielkosc kliki
-        'CONST_VERTICES'  : 5000,    #~ Ilosc wezlow
+        # 'CONST_CLIQUE'    : 3,      #~ Wielkosc kliki
+        'CONST_VERTICES'  : 1000,    #~ Ilosc wezlow
         'CONST_SIM_COUNT' : 1,      #~ Ilosc powtorzen symulacji
         'CONST_PRINT'     : False,  #~ Czy drukowac magnetyzacje co CONST_VERTICES krokow?
         'CONST_TIME'      : True,   #~ Czy przeprowadzac i drukowac wyniki diagnostyki?
         'CONST_FOLDER'    : "",     #~ Nic nie robi
         'CONST_OVERRIDEN' : False,  #~ Czy ma nadpisywac pliki podczas zapisywania wynikow
         'CONST_COMPRESS'  : True,   #~ Czy ma kompresowac dane przez zapisem    
-        'CONST_SIM_LONG'  : 14,     # ile wielkosci N ma liczyc
-        'CONST_PATH_BASIC_FOLDER' : 'Wyniki_pickle',
+        'CONST_SIM_LONG'  : 1000,     # ile wielkosci N ma liczyc
+        'CONST_PATH_BASIC_FOLDER' : 'Wyniki_lazy_meanK',
+        'CONST_MODEL'     : 'lazy', #'clique',
+        'CONST_LAZY_CUT'  : 0.003,
+        'CONST_MODEL_BASIC_VAL' : 'CONST_MEAN_k'
     }
 
-    START, STOP, STEP = 24.00, 30.00, 1.0
+    START, STOP, STEP = 10.00, 30.00, 1.0
     for p in np.arange(START,STOP + STEP,STEP):
         stg['CONST_EDGES']  = int(round(p * stg['CONST_VERTICES'] // 2, 0)) #~ Ilosc polaczen
         stg['CONST_MEAN_k'] = round(stg['CONST_EDGES']/stg['CONST_VERTICES']*2, 1)
