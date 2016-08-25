53ea8257b41aa895c130cc11fa27f1804f923cc0
Przemyslaw Kowalczyk <pirtim.pk@gmail.com>
2016-08-25 19:39:19 +0200

    changes

--- SimulateBarabasiLazy.py
+++ SimulateBarabasiLazy.py
@@ -8,10 +8,10 @@ def BA_lazy_faz(p):
         stg = {            
             'CONST_PRINT'     : False,  #~ Czy drukowac magnetyzacje co CONST_VERTICES krokow?
             'CONST_OVERRIDEN' : False,  #~ Czy ma nadpisywac pliki podczas zapisywania wynikow 
-            'CONST_VERTICES'  : 10000,    #~ Ilosc wezlow
+            'CONST_VERTICES'  : 100000,    #~ Ilosc wezlow
             'CONST_SIM_COUNT' : 1,      #~ Ilosc powtorzen symulacji
             'CONST_SIM_LONG'  : 10000,     # ile wielkosci N ma liczyc            
-            'CONST_PATH_BASIC_FOLDER' : 'now/complex_networks_sim/Wyniki_barabasi_lazy_fazowe',
+            'CONST_PATH_BASIC_FOLDER' : '~/now/complex_networks_sim/Wyniki_barabasi_lazy_fazowe',
             'CONST_MODEL'             : 'lazy',  
             'CONST_MODEL_BASIC_VAL'   : 'CONST_START_MAGNETIZATION',
             'CONST_NETWORK_MODEL'     : 'barabasi',
@@ -90,7 +90,7 @@ def main():
     clients = ipp.Client()
     dview = clients.load_balanced_view()
 
-    results = dview.map(BA_clique_normal(None), [2,3,4,5,6,7,8,9,10,11][::-1]*1)
+    results = dview.map(BA_lazy_faz(None), [0, 0.1,0.2,0.3,0.4,0.5,0.6,0.7,0.8,0.9,0.10]*10)
     print list(results) 
     # results = dview.map(BA_lazy_faz(None), [0.05, 0.1, 0.15, 0.2, 0.25, 0.3, 0.35, 0.4, 0.45, 0.5]*8)
     # print list(results)
