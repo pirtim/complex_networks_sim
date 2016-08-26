f3940bf9f08e79a3bc61dffebd5486772df68468
Przemyslaw Kowalczyk <pirtim.pk@gmail.com>
2016-08-26 11:13:05 +0200

    Changes

--- SimulateBarabasiLazy.py
+++ SimulateBarabasiLazy.py
@@ -8,14 +8,14 @@ def BA_lazy_faz(p):
         stg = {            
             'CONST_PRINT'     : False,  #~ Czy drukowac magnetyzacje co CONST_VERTICES krokow?
             'CONST_OVERRIDEN' : False,  #~ Czy ma nadpisywac pliki podczas zapisywania wynikow 
-            'CONST_VERTICES'  : 100000,    #~ Ilosc wezlow
+            'CONST_VERTICES'  : 1000,    #~ Ilosc wezlow
             'CONST_SIM_COUNT' : 1,      #~ Ilosc powtorzen symulacji
-            'CONST_SIM_LONG'  : 10000,     # ile wielkosci N ma liczyc            
+            'CONST_SIM_LONG'  : 500,     # ile wielkosci N ma liczyc            
             'CONST_PATH_BASIC_FOLDER' : '~/now/complex_networks_sim/Wyniki_barabasi_lazy_fazowe',
             'CONST_MODEL'             : 'lazy',  
             'CONST_MODEL_BASIC_VAL'   : 'CONST_START_MAGNETIZATION',
             'CONST_NETWORK_MODEL'     : 'barabasi',
-            'CONST_BARABASI_m'        : 4,            
+            'CONST_BARABASI_m'        : 4,
             'CONST_START_MAGNETIZATION' : x
         }
 
